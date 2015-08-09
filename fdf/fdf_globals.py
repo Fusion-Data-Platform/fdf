839d3f8eb3bcc02f8d2137eab5712327246ef3b7
David R. Smith <drsmith8@wisc.edu>
2015-08-09 15:23:31 -0400

    renamed dbaccess to logbook; split into logbook and logbook_globals

--- fdf/fdf_globals.py
+++ fdf/fdf_globals.py
@@ -11,4 +11,5 @@ if 'linux' in os.sys.platform:
 elif 'win32' in os.sys.platform:
     FDF_DIR = '.\\fdf\\'
 
+# is FDF_MDS_SERVER used anywhere?  -ds 8/9/15
 FDF_MDS_SERVER = 'skylark.pppl.gov:8501'
