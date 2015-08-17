cc352bfdf30c11799e1e0dc8c527a44ec34eaf40
David R. Smith <drsmith8@wisc.edu>
2015-08-17 15:03:48 -0400

    worked on MySQL connection

--- fdf/logbook.py
+++ fdf/logbook.py
@@ -3,8 +3,10 @@
 @author: drsmith-wisc
 """
 
-from logbook_globals import Session
+from logbook_globals import Session, Logbook
 
 # open a session
 session = Session()
 
+for shot, in session.query(Logbook.shot).filter_by(shot=140000):
+    print shot
--- fdf/logbook_globals.py
+++ fdf/logbook_globals.py
@@ -11,11 +11,20 @@ from sqlalchemy.orm import sessionmaker
 # tutorial: http://docs.sqlalchemy.org/en/rel_1_0/orm/tutorial.html
 # examples: http://docs.sqlalchemy.org/en/rel_1_0/orm/examples.html
 
+# mysql-python is the default DBAPI for the mysql dialect in sqlalchemy
+engine = create_engine('mysql+pymysql://drsmith:pfcworld@sql2008.pppl.gov/logbook')
+
 # base class for declarative class definitions
 Base = declarative_base()
 
-# mysql-python is the default DBAPI for the mysql dialect in sqlalchemy
-engine = create_engine('mysql+mysqldb://drsmith:pfcworld@sql2008.pppl.gov<:port>/<dbname>')
+class Logbook(Base):
+    __tablename__ = 'logbook'
+    
+    shot = Column(Integer, primary_key=True)
+    xp = Column(String(16))
+    datetime = Column(String(32), primary_key=True)
+    author = Column(String(20), primary_key=True)
+    entry = Column(String(256))
 
 # generate new Session class for conversing with database
 Session = sessionmaker(bind=engine)
