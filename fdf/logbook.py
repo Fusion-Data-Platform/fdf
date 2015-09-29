2e2d031a3c22b86667cd9ad5b75b1a0611e19992
David R. Smith <drsmith8@wisc.edu>
2015-09-29 15:24:17 -0400

    implemented pymssql in logbook.py

--- fdf/logbook.py
+++ fdf/logbook.py
@@ -3,19 +3,47 @@
 @author: drsmith-wisc
 """
 
-import pyodbc
+from os import getenv
+import pymssql
 
-driver = 'ODBC for MySQL (64 bit)'
 server = 'sql2008.pppl.gov\sql2008'
-connect_str = ('Driver={%s};'
-    'Server=%s;'
-    'UID=drsmith;'
-    'PWD=pfcworld;'
-    'Database=nstxlogs;'
-    'Port=62917' % (driver, server))
+username = getenv('USER')
+password = 'pfcworld'
+database = 'nstxlogs'
+port = '62917'
 
-connection = pyodbc.connect(connect_str)
-
+connection = pymssql.connect(server=server,
+                             user=username,
+                             password=password,
+                             database=database,
+                             port=port)
+                             
 cursor = connection.cursor()
 
+cursor.execute('SET ROWCOUNT 80')
 
+query = """
+select dbkey, username, rundate, shot, xp, topic, text, entered, voided from entries where VOIDED IS NULL and shot=140000 order by Shot asc, Entered asc
+"""
+cursor.execute(query)
+
+print(cursor.fetchone())
+print(cursor.fetchone())
+
+#import pyodbc
+#
+#driver = 'ODBC for MySQL (64 bit)'
+#server = 'sql2008.pppl.gov\sql2008'
+#connect_str = ('Driver={%s};'
+#    'Server=%s;'
+#    'UID=drsmith;'
+#    'PWD=pfcworld;'
+#    'Database=nstxlogs;'
+#    'Port=62917' % (driver, server))
+#
+#connection = pyodbc.connect(connect_str)
+#
+#cursor = connection.cursor()
+
+cursor.close()
+connection.close()
