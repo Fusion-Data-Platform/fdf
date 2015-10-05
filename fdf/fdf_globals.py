59d445e28f3c1c8f3f88d9fc8931b25b1c063673
David R. Smith <drsmith8@wisc.edu>
2015-10-05 11:25:52 -0400

    fixed bug with FDF_DIR in fdf_globals.py

--- fdf/fdf_globals.py
+++ fdf/fdf_globals.py
@@ -6,10 +6,13 @@ Created on Thu Jun 18 11:18:16 2015
 """
 import os
 
-if 'linux' in os.sys.platform:
-    FDF_DIR = './fdf/'
-elif 'win32' in os.sys.platform:
-    FDF_DIR = '.\\fdf\\'
+#if 'linux' in os.sys.platform:
+#    FDF_DIR = './fdf/'
+#elif 'win32' in os.sys.platform:
+#    FDF_DIR = '.\\fdf\\'
+
+# fixed bug where fdf crashed with script outside of fdf directory (DRS 10/5/15)
+FDF_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # is FDF_MDS_SERVER used anywhere  -ds 8/9/15
 #FDF_MDS_SERVER = 'skylark.pppl.gov:8501'
