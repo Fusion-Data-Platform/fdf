9d85655c6ad814974a15017c354a132715e9fe32
Howard Yuh <hyuh@pppl.gov>
2015-10-27 09:46:09 -0400

    properly deals with multidimensional signals w.r.t. axes

--- fdf/fdf_signal.py
+++ fdf/fdf_signal.py
@@ -69,7 +69,8 @@ class Signal(np.ndarray):
         #obj._root = root
         #obj._dim_of = dim_of
         obj._verbose = verbose
-        obj.slic = None
+        obj._slic = None
+        obj._empty = True
         #obj._name = name
         #not necessary but can be defined
         #obj.mdstree = mdstree
@@ -96,7 +97,7 @@ class Signal(np.ndarray):
 
     def __array_finalize__(self, obj):
 #        if self._verbose:
-#            print 'Called __array_finalize__:'
+#            print('Called __array_finalize__:')
 #            print '__array_finalize__: self is type %s  ' % type(self)
 #
 #            try:
@@ -104,13 +105,15 @@ class Signal(np.ndarray):
 #            except:
 #                print '__array_finalize__: self has undefined len'
 #
-#            print '__array_finalize__: self hasattr(self,"slic") is', hasattr(self,'slic')
-#            print '__array_finalize__: obj is type %s' % type(obj)
+#            print('__array_finalize__: self hasattr(self,"slic") is', hasattr(self,'slic'))
+#            print('__array_finalize__: obj is type ', type(obj))
 #            try:
 #                print '__array_finalize__: obj has len %s' % len(obj)
 #            except:
 #                print '__array_finalize__: obj has undefined len'
-#            print '__array_finalize__: self hasattr(obj,"slic") is', hasattr(obj,'slic')
+#            print('__array_finalize__: hasattr(obj,"_slic") is', hasattr(obj,"_slic"))
+#            if hasattr(obj,'_slic'):
+#                print('__array_finalize__: obj._slic is', obj._slic)
 
         if obj is None:
             return
@@ -122,29 +125,48 @@ class Signal(np.ndarray):
         self.axes_units = getattr(obj, 'axes_units', None)
         self.axes = getattr(obj, 'axes', None)
         self._verbose = getattr(obj, '_verbose', False)
-        self._transpose = getattr(obj, '_transpose', None)
+        #self._transpose = getattr(obj, '_transpose', None)
         self._parent = getattr(obj, '_parent', None)
+        self._empty = getattr(obj, '_empty', None)
+        #import pdb; pdb.set_trace()
+        #print(obj.__dict__)
         if hasattr(obj,'axes'):
             if obj.axes is not None:
                 for i, axis in enumerate(obj.axes):
-                    if hasattr(obj,'slic'):
+                    if hasattr(obj,'_slic'):
+                        if self._verbose:
+                            print('__array_finalize__: type(obj._slic) is  ', type(obj._slic))
+                            print('__array_finalize__: obj._slic is  ',obj._slic)
+
                         try:
-                            if len(obj.slic) >= i+1:
-                                if type(obj.slic) is slice:
-                                    setattr(self,axis,getattr(obj, axis)[obj.slic[i]])
-                                elif type(obj.slic) is tuple:
-                                    setattr(self,axis,getattr(obj, axis)[obj.slic[i],])
-                                else:
-                                    setattr(self,axis,getattr(obj, axis)[obj.slic])
+                            #1-D
+                            if type(obj._slic) is slice or type(obj._slic) is list:
+                                setattr(self,axis,getattr(obj, axis)[obj._slic])
+                            #>1-D
+                            elif type(obj._slic) is tuple:
+                                #if getattr(obj, axis).axes != []:
+                                #axes is multidimensional, build correct 
+                                _slicaxis=tuple([obj._slic[obj.axes.index(axisaxis)] for
+                                                 axisaxis in (getattr(obj, axis).axes + [axis])])
+                                if self._verbose:
+                                    print('__array_finalize__: Assigning axis  ',axis)
+                                    print('__array_finalize__: type(_slicaxis) is  ',type(_slicaxis))
+                                    print('__array_finalize__: _slicaxis is  ',_slicaxis)
+                                    print('__array_finalize__: axis shape is  ', getattr(obj, axis)[_slicaxis].shape)
+                                setattr(self,axis,getattr(obj, axis)[_slicaxis])
                             else:
-                                setattr(self,axis,getattr(obj, axis, None))
+                                if self._verbose:
+                                    print('_slic is neither slice, list, nor tuple type for ',axis)
+
+                            #elif type(obj._slic) is list:
+                            #    setattr(self,axis,getattr(obj, axis)[obj._slic])
                         except: #must not have a len(), e.g. int type
                             if self._verbose:
-                                print('slic does not have length, skipping attribute assignment')
+                                print('Exception: Axes parsing for ',axis,' failed')
                             pass
                     else:
                         setattr(self,axis,getattr(obj, axis, None))
-        self.slic=None
+        self._slic=None
 
 
     def __array_wrap__(self, out_arr, context=None):
@@ -156,34 +178,48 @@ class Signal(np.ndarray):
             print(context)
         return np.ndarray.__array_wrap__(self, out_arr, context)
 
+
     def __getitem__(self,index):
         '''
         self must be Signal class for this to be called, so therefore
-        must have the slic attribute
+        must have the _slic attribute. The _slic attribute preserves indexing for attributes
         '''
+        #This passes index to array_finalize after a new signal obj is created to assign axes
+        def parseindex(index, dims):
+             #format index to account for single elements and pad with appropriate slices.
+             if (type(index) is list or type(index) is slice):
+                 if dims <= 1: return index
+                 else: newindex=[index]
+             elif type(index) is tuple:
+                 newindex = [slice(i,i+1) if (type(i) is int or type(i) is long or type(i) is float) else i for i in index]
+             for i in range(dims-len(newindex)):newindex.append(slice(None))
+             return tuple(newindex)
+
         if self._verbose:
             print('Called __getitem__:')
-            self.slic=index
-            #print '   index is type %s' % type(index)
-            print('__getitem__: index is ', index)
-            print('__getitem__: self.slic is ', index)
-            #print '__getitem__: new is type %s' % type(new)
-            print('__getitem__: self is type %s' % type(self))
-            print('__getitem__: self has len %s ' % len(self))
 
+        self._slic=parseindex(index, self.ndim)
 
         #Get the data
-        if len(self) == 0:
+        if self._empty is True:
 #            try:
             data = self._root._get_mdsdata(self)
             self.resize(data.shape, refcheck=False)
             self[:] = data
+            self._empty=False
 #            except:
 #                print 'Something went wrong with getting data'
         #Exec userfunc if method defined:
+        if self._verbose:
+            print('__getitem__: index is ', index)
+            print('__getitem__: type(self._slic) is ', type(self._slic))
+            print('__getitem__: self._slic is ', self._slic)
+            #print '__getitem__: new is type %s' % type(new)
+            print('__getitem__: self is type %s' % type(self))
+            #print('__getitem__: self has len %s ' % len(self))
         return super(Signal,self).__getitem__(index)
 
-
+        
     def __getattr__(self, attribute):
         if attribute is '_parent':
             raise AttributeError("'{}' object has no attribute '{}'".format(
@@ -200,25 +236,28 @@ class Signal(np.ndarray):
     def __repr__(self):
         if self._verbose:
             print('Called custom __repr__')
-        if len(self) == 0:
+        if self._empty is True:
             data = self._root._get_mdsdata(self)
             self.resize(data.shape, refcheck=False)
             self[:] = data
+            self._empty=False
         return np.asarray(self).__repr__()
 
     def __str__(self):
         if self._verbose:
             print('Called custom __str__')
-        if len(self) == 0:
+        if self._empty is True:
             data = self._root._get_mdsdata(self)
             self.resize(data.shape, refcheck=False)
             self[:] = data
+            self._empty=False
         return np.asarray(self).__repr__()
 
     def __getslice__(self, start, stop):
         if self._verbose:
             print('Called __getslice__:')
-        """This solves a subtle bug, where __getitem__ is not called, and all
+        """
+        This solves a subtle bug, where __getitem__ is not called, and all
         the dimensional checking not done, when a slice of only the first
         dimension is taken, e.g. a[1:3]. From the Python docs:
         Deprecated since version 2.0: Support slice objects as parameters
@@ -247,7 +286,7 @@ class Signal(np.ndarray):
                 axis_ind = np.abs(values-axis_value[:]).argmin()
                 axis_inds = [axis_ind, axis_ind+1]
             slc[axis] = slice(axis_inds[0], axis_inds[1])
-        return self[slc]
+        return self[tuple(slc)]
 
 
     """
@@ -312,41 +351,3 @@ class Signal(np.ndarray):
             self[:]=data
         except:
             raise MdsError('Error populating signal from MDSplus')
-
-class RootContainer(object):
-    mds_servers = {
-        'nstx': 'skylark:8501'
-        }
-
-    def __init__(self, mdsConnectionsMax=20,**kwargs):
-            self.mdsConnectionsList=OrderedDict({x: None for x in range(20)})
-            for key,value in kwargs.iteritems():
-                setattr(self,key,value)
-
-    def _mdsget(self,shotnum,tree,node):
-        shotTree=str(shotnum).strip()+tree.lower().strip()
-        try:
-            connection=self.mdsConnectionsList[shotTree]
-            return connection.get(node).data()
-        except KeyError:
-            oldestConnection=self.mdsConnectionsList.popitem(last=False)
-            try:
-                oldestConnection[1].closeAllTrees
-                oldestConnection[1].openTree(tree,shotnum)
-                self.mdsConnectionsList.__setitem__(shotTree, oldestConnection)
-                return self.mdsConnectionsList[shotTree].get(node).data()
-            except AttributeError:
-                newConnection=mds.Connection(self.mds_servers['nstx'])
-                newConnection.openTree(tree,shotnum)
-                print('_mdsget: newConnection is %s' % newConnection)
-                self.mdsConnectionsList.__setitem__(shotTree, newConnection)
-                return self.mdsConnectionsList[shotTree].get(node).data()
-            except:
-                raise MdsError('Error opening tree or making MDS server connection')
-
-class shotContainer(object):
-    def __init__(self,shotnum=140000,tree='activespec',**kwargs):
-        self.mdsshot=shotnum
-        self.mdstree=tree
-        for key,value in kwargs.iteritems():
-            setattr(self,key,value)
